ba561a1edc51293afba910b97aaef949b03e081d
Derek Crew <crewderek@yahoo.com>
2024-02-18 23:29:04 -0700

    Prod URL changes

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
   ...CommonEnvironment,
   production: true,
   apiEnv: '/api',
-  authorizationQueryParams: `?client_id=${CommonEnvironment.clientId}&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin`,
-  logoutQueryParams: `?client_id=${CommonEnvironment.clientId}&logout_uri=http://localhost:4200/logout`,
+  authorizationQueryParams: `?client_id=${CommonEnvironment.clientId}&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https%3A%2F%2Fdnd3rd.scritch.ninja%2Flogin`,
+  logoutQueryParams: `?client_id=${CommonEnvironment.clientId}&logout_uri=https%3A%2F%2Fdnd3rd.scritch.ninja%2Flogout`,
 
   apiEnvUrl(){
     return `${CommonEnvironment.apiBaseURL}${this.apiEnv}`;
