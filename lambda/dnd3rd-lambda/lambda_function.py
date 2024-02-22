10b28799a169b1e4c81613d8fb85759cf5146275
Derek Crew <crewderek@yahoo.com>
2024-02-21 23:47:01 -0700

    Update lambda_function.py

--- lambda/dnd3rd-lambda/lambda_function.py
+++ lambda/dnd3rd-lambda/lambda_function.py
@@ -143,7 +143,7 @@ def create_ability(event, mysql_handler):
     return character.create_ability(items, cognito_user_id)
 
 def create_weapon(event, mysql_handler):
-    print('ima do thing')
+    print('ima do sathing')
     # global cognito_user_id
     # character = AbilityClient(mysql_handler)
     # items = event['body']
