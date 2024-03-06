0b2620dc26e81acd2bfe737d11935a776aff9f91
Derek Crew <crewderek@yahoo.com>
2024-03-05 18:11:41 -0700

    Update deploy.sh

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -1,17 +1,21 @@
 #/bin/sh
 # Reimport the API via the OpenAPI 3.0 doc
-aws apigatewayv2 reimport-api \
-    --body file://../swagger.yaml \
-    --fail-on-warnings \
-    --region us-west-2 \
-    --api-id 5t8dvdidxd \
-    --output json \
-    --no-cli-auto-prompt \
-    --no-cli-pager
+ls
 
-# Update an associated stage/deployment
-aws apigatewayv2 update-stage \
-    --api-id 5t8dvdidxd \
-    --region us-west-2 \
-    --stage-name api \
-    --no-cli-pager
+
+ls ../
+#aws apigatewayv2 reimport-api \
+#    --body file://../swagger.yaml \
+#    --fail-on-warnings \
+#    --region us-west-2 \
+#    --api-id 5t8dvdidxd \
+#    --output json \
+#    --no-cli-auto-prompt \
+#    --no-cli-pager
+#
+## Update an associated stage/deployment
+#aws apigatewayv2 update-stage \
+#    --api-id 5t8dvdidxd \
+#    --region us-west-2 \
+#    --stage-name api \
+#    --no-cli-pager
