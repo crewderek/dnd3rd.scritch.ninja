b6b2054cf36ad99a22340a22ae1757a07c4e390e
Derek Crew <crewderek@yahoo.com>
2024-03-05 19:20:52 -0700

    Updates to vars.

--- docs/deploy/replaceStringInYaml.sh
+++ docs/deploy/replaceStringInYaml.sh
@@ -3,4 +3,6 @@
 # Replace the environment variables in the swagger_deployment YAML
 #envsubst < ../swagger_deployment.yaml > ../swagger.yaml
 
-echo ${{ vars.COGNITO_AUDIENCE_1 }}
+#echo ${{ vars.COGNITO_AUDIENCE_1 }}
+
+cat ../swagger_deployment.yaml > ../swagger.yaml
