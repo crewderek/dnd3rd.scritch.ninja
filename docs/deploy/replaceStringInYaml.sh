5589fdc4073a586e4977fe0362bd380dd33d0be0
Derek Crew <crewderek@yahoo.com>
2024-03-05 18:08:18 -0700

    Testing deployment

--- docs/deploy/replaceStringInYaml.sh
+++ docs/deploy/replaceStringInYaml.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Replace the environment variables in the swagger_deployment YAML
-envsubst < ../swagger_deployment.yaml > swagger.yaml
+envsubst < ../swagger_deployment.yaml > ../swagger.yaml
+
+
+ls
+
+ls ../
