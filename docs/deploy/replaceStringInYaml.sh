b3c783c17a5f648d5abce2e550bf751e7c5c4e2a
Derek Crew <crewderek@yahoo.com>
2024-03-05 20:28:43 -0700

    Refactored Env Variable Replacement

--- docs/deploy/replaceStringInYaml.sh
+++ docs/deploy/replaceStringInYaml.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 original_file_path="../swagger_deployment.yaml"
+temp_file_path="../swagger.yaml.tmp"
 new_file_path="../swagger.yaml"
 
+# Create the temp file
+cat "$original_file_path" > "$temp_file_path"
+
 # Change $ref to something else so the envsubst command doesn't remove them
-sed -i 's/\$ref/\?ref/g' "$original_file_path"
+sed -i 's/\$ref/\?ref/g; s/\$request/\?request/g' "$temp_file_path"
 
 # Replace the environment variables in the swagger_deployment YAML
-envsubst < $original_file_path  > $new_file_path
+envsubst < $temp_file_path  > $new_file_path
 
 # Change the $refs back
-sed -i 's/\?ref/\$ref/g' "$new_file_path"
+sed -i 's/\?ref/\$ref/g; s/\?request/\$request/g' "$new_file_path"
+
+rm "$temp_file_path"
