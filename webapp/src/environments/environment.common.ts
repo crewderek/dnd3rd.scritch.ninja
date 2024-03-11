2af4fd47b6405aa975232bebb408560d3e2c3263
Derek Crew <crewderek@yahoo.com>
2024-03-10 23:18:16 -0600

    Changed API URL

--- webapp/src/environments/environment.common.ts
+++ webapp/src/environments/environment.common.ts
@@ -1,5 +1,5 @@
 export const CommonEnvironment = {
-  apiBaseURL: 'https://325zkzieal.execute-api.us-west-2.amazonaws.com',
+  apiBaseURL: 'https://v288jal5qi.execute-api.us-west-2.amazonaws.com',
   clientId: '7mgajsldg681cftcuu54bmkka1',
   devClientId: 'gt5l6jksir8h2utb1n2o8chq7',
   authorizationBaseUrl: 'https://dnd3rd-scritch-ninja.auth.us-west-2.amazoncognito.com',
